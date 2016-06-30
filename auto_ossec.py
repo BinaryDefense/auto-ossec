84b556ed4c39d2e9890fc40d5797402be6261380
TrustedSec <info@trustedsec.com>
2016-06-30 10:00:27 -0400

    autopep8 and add star support

--- auto_ossec.py
+++ auto_ossec.py
@@ -2,7 +2,7 @@
 #
 # This is the initial release of the ossec_client.py which can be byte compiled into an exe. This will
 # connect to the ossec_auto.py daemon that will automatically issue a key in order to pair the OSSEC
-# HIDS. 
+# HIDS.
 #
 # Also works with AlienVault.
 #
@@ -20,183 +20,220 @@ import time
 
 # try to import python-crypto
 try:
-	from Crypto.Cipher import AES
+    from Crypto.Cipher import AES
 
 except ImportError:
-	print ("[!] You need python-crypto in order for this module to work. If this is Ubuntu/Redhat - package name is python-crypto")
-	sys.exit()
+    print (
+        "[!] You need python-crypto in order for this module to work. If this is Ubuntu/Redhat - package name is python-crypto")
+    sys.exit()
 
 # check platform specific installs
 installer = ""
 if platform.system() == "Linux":
-	installer = "Linux"
+    installer = "Linux"
 
 if platform.system() == "Windows":
-	installer = "Windows"
+    installer = "Windows"
 
-if installer == "": 
-	print ("[!] Unable to determine operating system. Only supports Linux and Windows. Exiting..")
-	sys.exit()
+if installer == "":
+    print (
+        "[!] Unable to determine operating system. Only supports Linux and Windows. Exiting..")
+    sys.exit()
 
 #
 # NEED TO DEFINE THIS AS THE OSSEC SERVER HOST THAT IS RUNNING SERVER.PY
 #
-try: 
-	host = sys.argv[1]
+star = ""
+try:
+    host = sys.argv[1]
+    try:
+        star = sys.argv[2]
+    except:
+        pass
 
 except IndexError:
-	print ("""
+    print ("""
 ******************************************************
 Binary Defense Systems OSSEC Auto Enrollment
 
 In order for this to work, you need to point
 auto_ossec.exe to the OSSEC server that is
 listening. Note that default port is 9654
-but this can be changed in the source.  
+but this can be changed in the source.
 
-Usage: auto_ossec.exe <server_ip>
+Note that if you specify optional *, this will
+place a * for the IP address in the config and
+allow any IP address (for dynamic IP addresses).
+
+Example: auto_ossec.exe/.bin 192.168.5.5 *
+Usage: auto_ossec.exe <server_ip> <optional: *>
 
 *****************************************************
 		""")
-	sys.exit()
+    sys.exit()
+
 
 def aescall(secret, data, format):
 
-	# padding and block size
-	PADDING = '{'
-	BLOCK_SIZE = 32
-	# one-liner to sufficiently pad the text to be encrypted
-	pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-	
-	# random value here to randomize builds
-	a = 50 * 5
-	
-	# one-liners to encrypt/encode and decrypt/decode a string
-	EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-	DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-	
-	cipher = AES.new(secret)
-	
-	if format == "encrypt":
-                aes = EncodeAES(cipher, data)
-                return str(aes)
+    # padding and block size
+    PADDING = '{'
+    BLOCK_SIZE = 32
+    # one-liner to sufficiently pad the text to be encrypted
+    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
 
-	if format == "decrypt":
-                aes = DecodeAES(cipher, data)
-                return str(aes)
+    # random value here to randomize builds
+    a = 50 * 5
+
+    # one-liners to encrypt/encode and decrypt/decode a string
+    EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+    DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+
+    cipher = AES.new(secret)
+
+    if format == "encrypt":
+        aes = EncodeAES(cipher, data)
+        return str(aes)
+
+    if format == "decrypt":
+        aes = DecodeAES(cipher, data)
+        return str(aes)
 
 # this will grab the hostname and ip address and return it
+
+
 def grab_info():
-        try:
-                hostname = socket.gethostname()
-                return hostname #+ " " + ipaddr
-	
-        except Exception:
-                sys.exit()
+    try:
+        hostname = socket.gethostname()
+        return hostname  # + " " + ipaddr
+
+    except Exception:
+        sys.exit()
 try:
-        # secret key - if you change this you must change on ossec_auto server - would recommend this is the default published to git
-        secret = "(3j+-sa!333hNA2u3h@*!~h~2&^lk<!B"
-        # port for daemon
-        port = 9654 
-        # general length size of socket
-        size = 1024 
+    # secret key - if you change this you must change on ossec_auto server -
+    # would recommend this is the default published to git
+    secret = "(3j+-sa!333hNA2u3h@*!~h~2&^lk<!B"
+    # port for daemon
+    port = 9654
+    # general length size of socket
+    size = 1024
 
-        # loop through in case server isnt reachable
-        while 1:
-                try:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-                        s.connect((host,port))
-                        break
+    # loop through in case server isnt reachable
+    while 1:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, port))
+            break
 
-                except Exception: 
-                        print ("[!] Unable to connect to destination server. Re-trying in 10 seconds.")
-                        time.sleep(10)
-                        pass 
+        except Exception:
+            print (
+                "[!] Unable to connect to destination server. Re-trying in 10 seconds.")
+            time.sleep(10)
+            pass
 
-        print ("[*] Connected to auto enrollment server at IP: " + host)
-        # grab host info needed for ossec
-        data = grab_info()
-	
-        # encrypt the data
+    print ("[*] Connected to auto enrollment server at IP: " + host)
+    # grab host info needed for ossec
+    data = grab_info()
+
+    # encrypt the data
+    if star == "*":
+        data = "BDSOSSEC*" + data.rstrip()
+    else:
         data = "BDSOSSEC" + data.rstrip()
-        data = aescall(secret, data, "encrypt")
-        print ("[*] Pulled hostname and IP, encrypted data, and now sending to server.")
-        s.send(data) 
-        data = s.recv(size) 
-        # this is our ossec key
-        print ("[*] We received our new pairing key for OSSEC, closing server connection.")
-        data = aescall(secret, data, "decrypt")
-        # close socket
-        s.close()
-	
-        # path variables for OSSEC
-        if os.path.isdir("C:\\Program Files (x86)\\ossec-agent"): path = "C:\\Program Files (x86)\\ossec-agent"
-        if os.path.isdir("C:\\Program Files\\ossec-agent"): path = "C:\\Program Files\\ossec-agent"
-        if os.path.isdir("/var/ossec/"): path = "/var/ossec/"
-        if path == "": sys.exit()
-        print ("[*] Removing any old keys.")
-        os.chdir(path)
-	
-        if installer == "Windows":
-                if os.path.isfile("client.keys"): os.remove("client.keys")
-                # import the key with the key presented from the server daemon
-                filewrite = file(path + "\\client.keys", "w")
+    data = aescall(secret, data, "encrypt")
+    print (
+        "[*] Pulled hostname and IP, encrypted data, and now sending to server.")
+    s.send(data)
+    data = s.recv(size)
+    # this is our ossec key
+    print (
+        "[*] We received our new pairing key for OSSEC, closing server connection.")
+    data = aescall(secret, data, "decrypt")
+    # close socket
+    s.close()
 
-        if installer == "Linux":
-                if os.path.isfile(path + "/etc/client.keys"): os.remove("etc/client.keys")
-                filewrite = file(path + "/etc/client.keys", "w")
+    # path variables for OSSEC
+    if os.path.isdir("C:\\Program Files (x86)\\ossec-agent"):
+        path = "C:\\Program Files (x86)\\ossec-agent"
+    if os.path.isdir("C:\\Program Files\\ossec-agent"):
+        path = "C:\\Program Files\\ossec-agent"
+    if os.path.isdir("/var/ossec/"):
+        path = "/var/ossec/"
+    if path == "":
+        sys.exit()
+    print ("[*] Removing any old keys.")
+    os.chdir(path)
+
+    if installer == "Windows":
+        if os.path.isfile("client.keys"):
+            os.remove("client.keys")
+        # import the key with the key presented from the server daemon
+        filewrite = file(path + "\\client.keys", "w")
+
+    if installer == "Linux":
+        if os.path.isfile(path + "/etc/client.keys"):
+            os.remove("etc/client.keys")
+        filewrite = file(path + "/etc/client.keys", "w")
+
+    data = base64.b64decode(data)
+    filewrite.write(data)
+    filewrite.close()
+    # subprocess.Popen("echo y | manage_agents.exe -i %s" % data,
+    # stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+    print ("[*] Successfully imported the new pairing key.")
+    print ("[*] Stopping the OSSEC service, just in case its running.")
+    # stop the service if it is
+    if installer == "Windows":
+        subprocess.Popen('net stop "OSSEC HIDS"', stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, shell=True).wait()
+
+    if installer == "Linux":
+        subprocess.Popen("service ossec stop", stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, shell=True).wait()
+
+    print ("[*] Modifying ossec.conf to incorporate server host IP address.")
+    # make sure we modify the ossec.conf
+
+    if installer == "Windows":
+        data = file(path + "\\ossec.conf", "r").read()
+        if not "<server-ip>%s</server-ip>" % (host) in data:
+            filewrite = file(path + "\\ossec.conf", "a")
+            filewrite.write("\n")
+            filewrite.write(" <ossec_config>")
+            filewrite.write("   <client>")
+            filewrite.write("      <server-ip>%s</server-ip>" % (host))
+            filewrite.write("   </client>")
+            filewrite.write(" </ossec_config>")
+            filewrite.close()
+
+    if installer == "Linux":
+        data = file(path + "etc/ossec.conf", "r").read()
+        if not "<server-ip>%s</server-ip>" % (host) in data:
+            filewrite = file(path + "etc/ossec.conf", "a")
+            filewrite.write("\n")
+            filewrite.write(" <ossec_config>")
+            filewrite.write("   <client>")
+            filewrite.write("    <server-ip>%s</server-ip>" % (host))
+            filewrite.write("   </client>")
+            filewrite.write(" </ossec_config>")
+            filewrite.close()
+
+    # start the service
+    if installer == "Windows":
+        subprocess.Popen('net start "OSSEC HIDS"', stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, shell=True).wait()
+
+    if installer == "Linux":
+        subprocess.Popen("service ossec start", stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, shell=True).wait()
+
+    print (
+        "[*] Finished. Started the OSSEC service. Auto Enrollment for OSSEC is now finished.")
 
 
-        data = base64.b64decode(data)
-        filewrite.write(data)
-        filewrite.close()
-        #subprocess.Popen("echo y | manage_agents.exe -i %s" % data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-        print ("[*] Successfully imported the new pairing key.")
-
-        print ("[*] Stopping the OSSEC service, just in case its running.")
-        # stop the service if it is
-        if installer == "Windows":
-                subprocess.Popen('net stop "OSSEC HIDS"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-
-        if installer == "Linux":
-                subprocess.Popen("service ossec stop", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-
-        print ("[*] Modifying ossec.conf to incorporate server host IP address.")
-        # make sure we modify the ossec.conf
-
-        if installer == "Windows":
-                data = file(path + "\\ossec.conf", "r").read()
-                if not "<server-ip>%s</server-ip>" % (host) in data:
-                        filewrite = file(path + "\\ossec.conf", "a")
-                        filewrite.write("\n")
-                        filewrite.write(" <ossec_config>")
-                        filewrite.write("   <client>")
-                        filewrite.write("      <server-ip>%s</server-ip>" % (host))
-                        filewrite.write("   </client>")
-                        filewrite.write(" </ossec_config>")
-                        filewrite.close()
-
-        if installer == "Linux":
-                data = file(path + "etc/ossec.conf", "r").read()
-                if not "<server-ip>%s</server-ip>" % (host) in data:
-                        filewrite = file(path + "etc/ossec.conf", "a")
-                        filewrite.write("\n")
-                        filewrite.write(" <ossec_config>")
-                        filewrite.write("   <client>")
-                        filewrite.write("    <server-ip>%s</server-ip>" % (host))
-                        filewrite.write("   </client>")
-                        filewrite.write(" </ossec_config>")
-                        filewrite.close()
-
-        # start the service
-        if installer == "Windows":
-                subprocess.Popen('net start "OSSEC HIDS"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait() 
-
-        if installer == "Linux":
-                subprocess.Popen("service ossec start", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-
-        print ("[*] Finished. Started the OSSEC service. Auto Enrollment for OSSEC is now finished.")
-
+except KeyboardInterrupt:
+    print ("Sounds good.. Abording Auto-OSSEC...")
+    sys.exit()
 except Exception as error:
-        print ("[*] Something did not complete. Does this system have Internet access?")
-        print (error)
+    print (
+        "[*] Something did not complete. Does this system have Internet access?")
+    print (error)
--- auto_server.py
+++ auto_server.py
@@ -14,167 +14,190 @@ import sys
 
 # check python crypto library
 try:
-	from Crypto.Cipher import AES
+    from Crypto.Cipher import AES
 
 except ImportError:
-	print "[!] python-crypto not installed. Run 'apt-get install python-pycrypto pexpect' to fix."
-	sys.exit()
+    print "[!] python-crypto not installed. Run 'apt-get install python-pycrypto pexpect' to fix."
+    sys.exit()
 
 import base64
 import thread
 
 # check pexpect library
 try:
-	import pexpect
+    import pexpect
 except ImportError:
-	print "[!] pexpect not installed. Run apt-get install pexpect to fix."
-	sys.exit()
+    print "[!] pexpect not installed. Run apt-get install pexpect to fix."
+    sys.exit()
 
 import time
 import socket
 import os
 
+
 class service(SocketServer.BaseRequestHandler):
 
-	def handle(self):
-		# parse OSSEC hids client certificate
-		def parse_client(hostname, ipaddr):
-			child = pexpect.spawn("/var/ossec/bin/manage_agents")
-			child.expect("Choose your action")
-			child.sendline("a")
-			child.expect("for the new agent")
-			child.sendline(hostname)
-			i = child.expect(['IP Address of the new agent', 'already present'])
-							
-			# if we haven't already added the hostname
-			if i == 0:
-				child.sendline(ipaddr)
-				child.expect("for the new agent")
-				child.sendline("")
-				for line in child: 
-					# pull id
-					if "[" in line: id = line.replace("[", "").replace("]", "").replace(":", "").rstrip()
-					break
-				child.expect("Confirm adding it?")
-				child.sendline("y")
-				child.sendline("")
-				child.sendline("q")
-				child.close()
+    def handle(self):
+        # parse OSSEC hids client certificate
+        def parse_client(hostname, ipaddr):
+            child = pexpect.spawn("/var/ossec/bin/manage_agents")
+            child.expect("Choose your action")
+            child.sendline("a")
+            child.expect("for the new agent")
+            child.sendline(hostname)
+            i = child.expect(
+                ['IP Address of the new agent', 'already present'])
 
-				child = pexpect.spawn("/var/ossec/bin/manage_agents -e %s" % (id))
-				for line in child:
-					key = line.rstrip()
+            # if we haven't already added the hostname
+            if i == 0:
+                child.sendline(ipaddr)
+                child.expect("for the new agent")
+                child.sendline("")
+                for line in child:
+                    # pull id
+                    if "[" in line:
+                        id = line.replace(
+                            "[", "").replace("]", "").replace(":", "").rstrip()
+                    break
+                child.expect("Confirm adding it?")
+                child.sendline("y")
+                child.sendline("")
+                child.sendline("q")
+                child.close()
 
-				return key
-			
-			# if we have a duplicate hostname
-			else:
-				child.close()
-				child = pexpect.spawn("/var/ossec/bin/manage_agents -l")
-				for line in child:
-					line = line.rstrip()
-					if hostname in line:
-						id = line.split(",")[0].replace("ID: ", "").replace("   ", "").rstrip()
-						break
-				child.close()
-				subprocess.Popen("/var/ossec/bin/manage_agents -r %s" % (id), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-				return 0
+                child = pexpect.spawn(
+                    "/var/ossec/bin/manage_agents -e %s" % (id))
+                for line in child:
+                    key = line.rstrip()
 
-		
-		# main AES encrypt and decrypt function with 32 block size padding
-	        def aescall(secret, data, format):
+                return key
 
-		    # padding and block size
-	            PADDING = '{'
-	            BLOCK_SIZE = 32
+            # if we have a duplicate hostname
+            else:
+                child.close()
+                child = pexpect.spawn("/var/ossec/bin/manage_agents -l")
+                for line in child:
+                    line = line.rstrip()
+                    if hostname in line:
+                        id = line.split(",")[0].replace(
+                            "ID: ", "").replace("   ", "").rstrip()
+                        break
+                child.close()
+                subprocess.Popen("/var/ossec/bin/manage_agents -r %s" %
+                                 (id), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+                return 0
 
-	            # one-liner to sufficiently pad the text to be encrypted
-	            pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-	
-	            # random value here to randomize builds
-	            a = 50 * 5
-	
-	            # one-liners to encrypt/encode and decrypt/decode a string
-	            # encrypt with AES, encode with base64
-	            EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-	            DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-	
-	            cipher = AES.new(secret)
-	
-	            if format == "encrypt":
-	                    aes = EncodeAES(cipher, data)
-	                    return str(aes)
-	
-	            if format == "decrypt":
-       	            	    aes = DecodeAES(cipher, data)
-        	            return str(aes)
+        # main AES encrypt and decrypt function with 32 block size padding
+        def aescall(secret, data, format):
 
-		# recommend changing this - if you do, change auto_ossec.py as well - - would recommend this is the default published to git
-		secret = "(3j+-sa!333hNA2u3h@*!~h~2&^lk<!B"
-        	print "Client connected with ", self.client_address
-		try:	
-			data = self.request.recv(1024)
-		 	if data != "":
-				try:
-					data = aescall(secret, data, "decrypt")
+            # padding and block size
+            PADDING = '{'
+            BLOCK_SIZE = 32
 
-					# if this section clears -we know that it is a legit request, has been decrypted and we're ready to rock
-					if "BDSOSSEC" in data: 
+            # one-liner to sufficiently pad the text to be encrypted
+            pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
 
-						# write a lock file to check later on with our threaded process to restart OSSEC if needed every 10 minutes - if lock file is present then it will trigger a restart of OSSEC server
-						if not os.path.isfile("lock"): 
-							filewrite = file("lock", "w")
-							filewrite.write("lock")
-							filewrite.close()
+            # random value here to randomize builds
+            a = 50 * 5
 
-						# strip identifier
-						data = data.replace("BDSOSSEC", "")
-						hostname = data
+            # one-liners to encrypt/encode and decrypt/decode a string
+            # encrypt with AES, encode with base64
+            EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+            DecodeAES = lambda c, e: c.decrypt(
+                base64.b64decode(e)).rstrip(PADDING)
 
-						# pull the true IP, not the NATed one if they are using VMWare
-						ipaddr = self.client_address[0]
+            cipher = AES.new(secret)
 
-						# here if the hostname was already used, we need to remove it and call it again
-						data = parse_client(hostname, ipaddr)
-						if data == 0: data = parse_client(hostname, ipaddr)
-						print "[*] Provisioned new key for hostname: %s with IP of: %s" % (hostname, ipaddr)
-						data = aescall(secret, data, "encrypt")
-						print "[*] Sending new key to %s: " % (ipaddr) + data
-			                        self.request.send(data)
+            if format == "encrypt":
+                aes = EncodeAES(cipher, data)
+                return str(aes)
 
-				except Exception, e:
-					print e
-					pass
+            if format == "decrypt":
+                aes = DecodeAES(cipher, data)
+                return str(aes)
 
-		except Exception, e:
-			print e
-			pass
+        # recommend changing this - if you do, change auto_ossec.py as well - -
+        # would recommend this is the default published to git
+        secret = "(3j+-sa!333hNA2u3h@*!~h~2&^lk<!B"
+        print "Client connected with ", self.client_address
+        try:
+            data = self.request.recv(1024)
+            if data != "":
+                try:
+                    data = aescall(secret, data, "decrypt")
 
-        	print "Pairing complete. Terminating connection to client."
-       		self.request.close()
+                    # if this section clears -we know that it is a legit
+                    # request, has been decrypted and we're ready to rock
+                    if "BDSOSSEC" in data:
 
-# this waits 5 minutes to check if new ossec agents have been deployed, if so it restarts the server
+                        # write a lock file to check later on with our threaded
+                        # process to restart OSSEC if needed every 10 minutes -
+                        # if lock file is present then it will trigger a
+                        # restart of OSSEC server
+                        if not os.path.isfile("lock"):
+                            filewrite = file("lock", "w")
+                            filewrite.write("lock")
+                            filewrite.close()
+
+                        # strip identifier
+                        data = data.replace("BDSOSSEC", "")
+                        hostname = data
+
+                        # pull the true IP, not the NATed one if they are using
+                        # VMWare
+                        ipaddr = self.client_address[0]
+
+                        # here if the hostname was already used, we need to
+                        # remove it and call it again
+                        data = parse_client(hostname, ipaddr)
+                        if data == 0:
+                            data = parse_client(hostname, ipaddr)
+                        print "[*] Provisioned new key for hostname: %s with IP of: %s" % (hostname, ipaddr)
+                        data = aescall(secret, data, "encrypt")
+                        print "[*] Sending new key to %s: " % (ipaddr) + data
+                        self.request.send(data)
+
+                except Exception, e:
+                    print e
+                    pass
+
+        except Exception, e:
+            print e
+            pass
+
+        print "Pairing complete. Terminating connection to client."
+        self.request.close()
+
+# this waits 5 minutes to check if new ossec agents have been deployed, if
+# so it restarts the server
+
+
 def ossec_monitor():
-	time.sleep(300)
-	if os.path.isfile("lock"):
-		os.remove("lock")
-		print "[*] New OSSEC agent added - triggering restart of service to add.."
-		subprocess.Popen("service ossec restart", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+    time.sleep(300)
+    if os.path.isfile("lock"):
+        os.remove("lock")
+        print "[*] New OSSEC agent added - triggering restart of service to add.."
+        subprocess.Popen("service ossec restart", stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, shell=True).wait()
 
-class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer): pass
 
-print "[*] The auto enrollment OSSEC Server is now listening on 9654" 
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+print "[*] The auto enrollment OSSEC Server is now listening on 9654"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # set is so that when we cancel out we can reuse port
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # bind to all interfaces on port 10900
-t = ThreadedTCPServer(('',9654), service)
+
+ThreadedTCPServer.allow_reuse_address = True
+t = ThreadedTCPServer(('', 9654), service)
 # start the server and listen forever
 try:
-	# start a threaded counter
-	thread.start_new_thread(ossec_monitor,())
+    # start a threaded counter
+    thread.start_new_thread(ossec_monitor, ())
 
-	t.serve_forever()
+    t.serve_forever()
 
 except KeyboardInterrupt:
-	print "[*] Exiting the automatic enrollment OSSEC daemon"
+    print "[*] Exiting the automatic enrollment OSSEC daemon"
